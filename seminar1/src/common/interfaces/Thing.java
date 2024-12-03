41ae0d47d5d7832017666fbc0036eeb97bd71777
Dmitriy_Demin <d.demin@inbox.ru>
2024-12-03 20:24:36 +0400

    sem1

---
+++ seminar1/src/common/interfaces/Thing.java
@@ -0,0 +1,14 @@
+package common.interfaces;
+
+/**
+ *  Вещь
+ */
+public interface Thing {
+
+    /**
+     *  Получить наименование вещи
+     * @return наименование вещи
+     */
+    String getName();
+
+}
